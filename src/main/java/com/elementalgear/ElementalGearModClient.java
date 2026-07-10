43f30fb7eff6768a89188dff959989aba118db25
mattroberts8388-dot <mattroberts8388@gmail.com>
2026-07-10 17:16:11 -0400

    Add src/main/java/com/elementalgear/ElementalGearModClient.java

---
+++ src/main/java/com/elementalgear/ElementalGearModClient.java
@@ -0,0 +1,10 @@
+package com.elementalgear;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ElementalGearModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // No custom entities to register renderers for.
+    }
+}
